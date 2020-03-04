5a41df914bce6c23228355664e7e195e8e5b38e4
thbeaumo <thbeaumo@student.42.fr>
2020-03-04 16:08:31 +0100

    ft_server done ?

--- srcs/script.sh
+++ srcs/script.sh
@@ -2,7 +2,7 @@ service php7.3-fpm start
 service nginx start
 service mysql start
 mysql -u root -p="root" < handledatabase
-sudo ln -s /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/localhost /etc/nginx/sites-enabled/localhost
 service nginx restart
 service mysql stop
 service mysql start
